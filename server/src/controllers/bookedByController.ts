e9b4654f65b40cc61427a6088cf33aff7872fa03
devdesignerprakash <devdesignerprakash@gmail.com>
2025-09-15 13:13:27 +0545

    feat: get all booked with details api completed

--- server/src/controllers/bookedByController.ts
+++ server/src/controllers/bookedByController.ts
@@ -1,9 +1,10 @@
 import { BookedByServices } from "../services/bookedBy.services";
 import { BookedUserDTO } from "../DTOs/booking.dto";
-import { Body, Controller, Middlewares, Post, Route, SuccessResponse, Tags } from "tsoa";
+import { Body, Controller, Get, Middlewares, Path, Post, Route, SuccessResponse, Tags } from "tsoa";
 import { Booking } from "../models/booking.entity";
 import { BookingStatus } from "../utils/status.enum";
 import { verifyToken } from "../utils/token";
+import { BookedByUser } from "../models/bookedbyUser.entity";
 
 @Route('bookedBy')
 @Tags("BookedBy")
@@ -37,6 +38,37 @@ public async createBooking(@Body()body:BookedUserDTO) {
         this.setStatus(500)
         return {message:"Internal server error"}
     }
-    
+}
+/**
+ *get all booked
+ */
+@Get("/all-booked")
+@SuccessResponse("200","all booked is fetched")
+public async getAllBooked(){
+    try{
+        const allBooked= await BookedByUser.find({relations:{booking:true,user:true},select:{
+            id:true,
+            user:{
+                id:true,
+                fullName:true,
+                phoneNumber:true
+            },
+            booking:{
+                id:true,
+                bookingStatus:true,
+                date:true,
+                start_time:true,
+                end_time:true,
+            }
+        }})
+        this.setStatus(200)
+        return ({data:allBooked})
+    }
+    catch(error){
+        console.log("get all booked controller error", error)
+        this.setStatus(500)
+        return {message:"Internal server error"}
+    }
+
 }
 }
